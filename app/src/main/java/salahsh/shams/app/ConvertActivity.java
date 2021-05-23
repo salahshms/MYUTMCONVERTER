1fe3fc18714d4c34517f0029ca6ecffe14fd9301
salah shams <salahshms@gmail.com>
2021-05-23 21:54:35 +0430

    last revision 2-3-1400

--- app/src/main/java/salahsh/shams/app/ConvertActivity.java
+++ app/src/main/java/salahsh/shams/app/ConvertActivity.java
@@ -1,22 +1,16 @@
 package salahsh.shams.app;
 
-import android.Manifest;
-import android.content.DialogInterface;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import salahsh.shams.R;
 
@@ -34,8 +28,8 @@ public class ConvertActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.share,R.id.map,R.id.aboutus)
+        new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.share, R.id.map, R.id.aboutus)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
